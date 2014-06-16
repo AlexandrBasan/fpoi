e470ecafef1d1aa6362ec54826fe0f48195d9256
Proalab <alexandr.basan@gmail.com>
2014-06-16 17:13:43 +0300

    fpoi

--- app/helpers/objectbases_helper.rb
+++ app/helpers/objectbases_helper.rb
@@ -1,2 +1,10 @@
 module ObjectbasesHelper
+
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(objectbase, options = { size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(objectbase.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: objectbase.name, class: "gravatar")
+  end
 end
