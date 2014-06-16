b93785c3f587bca5214a6ea819beb73fa13af6d4
Proalab <alexandr.basan@gmail.com>
2014-06-16 14:47:03 +0300

    3

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ SampleApp::Application.routes.draw do
   resources :sessions,      only: [:new, :create, :destroy]
   resources :blogs
   root 'static_pages#home'
-  match '/privacy_and_security', to: 'static_pages#privacy_and_security', via: 'get'
-  match '/terms_of_use', to: 'static_pages#terms_of_use', via: 'get'
+  #match '/privacy_and_security', to: 'static_pages#privacy_and_security', via: 'get'
+  #match '/terms_of_use', to: 'static_pages#terms_of_use', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
